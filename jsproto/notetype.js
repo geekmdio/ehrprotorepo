bd8cae19050257d6735ae20812e31813e27e80ab
Travis Nesbit <travis@geekmd.io>
2018-08-05 11:04:59 -0700

    Created a script to make for different languages

---
+++ jsproto/notetype.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.ehr.clinicalnote.NoteType');
+
+/**
+ * @enum {number}
+ */
+proto.ehr.clinicalnote.NoteType = {
+  NO_NOTE_TYPE: 0,
+  FOLLOW_UP: 1,
+  PHONE_CALL: 2,
+  CONTINUED_CARE_DOCUMENTATION: 4,
+  REFILL_DOCUMENTATION: 5,
+  INTAKE: 6,
+  PROCEDURE: 7,
+  HISTORY_AND_PHYSICAL: 8
+};
+
